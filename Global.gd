554cf3b6cc6c47f8e24bfe2c702a0ca8f3808ccd
axvg <91773368+axvg@users.noreply.github.com>
2023-06-18 17:29:39 -0500

    add global singleton

---
+++ Global.gd
@@ -0,0 +1,25 @@
+extends Node
+
+var levels = [
+				'res://levels/Level1.tscn',
+#				'res://levels/Level2.tscn'
+			]
+
+var current_level
+
+var start_screen = 'res://ui/start/StartScreen.tscn'
+var end_screen = 'res://ui/end/EndScreen.tscn'
+
+func new_game():
+	current_level = -1
+	next_level()
+
+func game_over():
+	get_tree().change_scene(end_screen)
+
+func next_level():
+	current_level += 1
+	if current_level >= Global.levels.size():
+		game_over()
+	else:
+		get_tree().change_scene(levels[current_level])
